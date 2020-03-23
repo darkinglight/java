224460090f61a68572fd38b72d8acc6b9416f89c
light <chenyue875104283@126.com>
2020-03-23 09:45:13 +0800

    add SweetShop

---
+++ jvm/AutoEncode.java
@@ -0,0 +1,10 @@
+package com.light.test;
+
+public class AutoEncode {
+    public static void main(String[] args) {
+        Integer a = 321;
+        Integer b = 321;
+        
+        System.out.println(a == b);
+    }
+}
---
+++ jvm/com/light/test/SweetShop.java
@@ -0,0 +1,29 @@
+package com.light.test;
+
+class Cookie {
+    static { System.out.println("Loading Cookie"); }
+}
+
+class Gum {
+    static { System.out.println("Loading Gum"); }
+}
+
+class Candy {
+    static { System.out.println("Loading Candy"); }
+}
+
+public class SweetShop {
+    public static void main(String[] args) {
+        System.out.println("inside main");
+        new Candy();
+        System.out.println("After creating Candy");
+        try {
+            Class.forName("com.light.test.Gum");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Couldn't find Gum");
+        }
+        System.out.println("After Class.forName(\"Gum\")");
+        new Cookie();
+        System.out.println("After creating Cookie");
+    }
+}
