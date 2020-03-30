4bd385e01ae4f29453429113d64cadfd0ea08d54
chenyuejun <chenyuejun@qiyi.com>
2020-03-30 22:46:04 +0800

    thread

--- maven/boot/src/main/java/Example.java
+++ maven/boot/src/main/java/Example.java
@@ -2,11 +2,22 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @RestController
 @EnableAutoConfiguration
 public class Example {
     @RequestMapping("/")
     String home() {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.submit(() -> {
+            System.out.println("sub start");
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {}
+            System.out.println("sub end");
+        });
         return "Home";
     }
 
